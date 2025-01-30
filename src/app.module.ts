cd10b0d77f780df6e42cd2851198a5beb9785588
ivankdev-fullstack <ivan.kononenko.dev@gmail.com>
2025-01-30 23:09:37 +0200

    feat: add and connect user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { ReportModule } from './report/report.module';
+import { UserModule } from './user/user.module';
+import { User } from './user/entity/user.entity';
 
 @Module({
-  imports: [UserModule, ReportModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule,
+    ReportModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
