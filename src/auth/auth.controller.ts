0d288ceef23a8ad7d06d34a2a8674f194977bb08
ivankdev-fullstack <ivan.kononenko.dev@gmail.com>
2025-02-01 00:37:22 +0200

    feat: add user cookie auth session and /me route for auth check

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,56 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  Session,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
 import { CreateUserDto } from '../user/entity/user.dto';
 import { User } from '../user/entity/user.entity';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
+
+  @Get('me')
+  public async me(@Session() session: any): Promise<User> {
+    if (!session.userId) {
+      throw new UnauthorizedException('Not authorized.');
+    }
+    return this.userService.getById(session.userId);
+  }
 
   @Post('signin')
-  public async signinUser(@Body() body: CreateUserDto): Promise<User> {
-    return this.authService.signinUser(body);
+  @HttpCode(200)
+  public async signinUser(
+    @Body() body: CreateUserDto,
+    @Session() session: any,
+  ): Promise<User> {
+    const user = await this.authService.signinUser(body);
+    session.userId = user.id;
+    return user;
   }
 
   @Post('signup')
-  public async signupUser(@Body() body: CreateUserDto): Promise<User> {
-    return this.authService.signupUser(body);
+  public async signupUser(
+    @Body() body: CreateUserDto,
+    @Session() session: any,
+  ): Promise<User> {
+    const user = await this.authService.signupUser(body);
+    session.userId = user.id;
+    return user;
+  }
+
+  @Post('signout')
+  @HttpCode(200)
+  public async signoutUser(@Session() session: any): Promise<void> {
+    session.userId = null;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,26 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const cookieSession = require('cookie-session');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['userId'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+  (app as any).set('etag', false);
+  app.use((req, res, next) => {
+    res.removeHeader('x-powered-by');
+    res.removeHeader('date');
+    next();
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
