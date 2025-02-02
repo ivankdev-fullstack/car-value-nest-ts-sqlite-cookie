b2a677d09722e3193d8c4f02db2efd2fd8b1121f
ivankdev-fullstack <ivan.kononenko.dev@gmail.com>
2025-02-02 22:19:26 +0200

    feat: add create report endpoint

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { CreateReportDto } from './entity/report.dto';
+import { ReportService } from './report.service';
 
 @Controller('reports')
-export class ReportController {}
+export class ReportController {
+  constructor(private readonly reportService: ReportService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  public async createReport(@Body() body: CreateReportDto) {
+    return this.reportService.create(body);
+  }
+}
--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './entity/report.dto';
+import { Report } from './entity/report.entity';
 
 @Injectable()
-export class ReportService {}
+export class ReportService {
+  constructor(
+    @InjectRepository(Report)
+    private readonly reportRepository: Repository<Report>,
+  ) {}
+
+  public async create(data: CreateReportDto) {
+    const report = this.reportRepository.create(data);
+    return this.reportRepository.save(report);
+  }
+}
